d3a3a19e8e05d6e0c0b04bec89c172dd48fb4506
Jun Tan <tanjunyuan@hotmail.co.uk>
2018-01-23 14:35:07 +0800

    added more DB functions, object-oriented a bit, considering shift to django

--- db.py
+++ db.py
@@ -10,10 +10,26 @@ class DB:
         c.execute('CREATE TABLE IF NOT EXISTS artists '
                   '("id" TEXT NOT NULL UNIQUE, '
                   '"name" TEXT, "tracks" BLOB)')
+
+        # place our audio features into the tracks table
         c.execute('CREATE TABLE IF NOT EXISTS tracks '
-                  '("id" TEXT NOT NULL UNIQUE, '
-                  '"name" TEXT, '
-                  'artist_id TEXT NOT NULL UNIQUE, '
+                  '(id TEXT NOT NULL UNIQUE, '
+                  'name TEXT, '
+                  'artist_id TEXT NOT NULL UNIQUE,'
+                  'artist_name TEXT,'
+                  'album_id TEXT,'
+                  'album_name TEXT,'
+                  'danceability REAL,'
+                  'energy REAL,'
+                  'key REAL,'
+                  'loudness REAL,'
+                  'mode REAL,'
+                  'speechiness REAL,'
+                  'acousticness REAL,'
+                  'instrumentalness REAL,'
+                  'liveness REAL,'
+                  'valence REAL,'
+                  'tempo REAL,'
                   'FOREIGN KEY (artist_id) REFERENCES artists(id))')
 
         self.db = conn
@@ -21,23 +37,38 @@ class DB:
         self.db_path = db_path
 
 
-    def get_tracks_by_artist_id(self, artist_id):
+    def getTracksByArtistId(self, artist_id: str):
         self.db_cursor.execute('SELECT * FROM artists WHERE id = ?', artist_id)
         rows = self.db_cursor.fetchall()
         return rows
 
-    def set_tracks_for_artist_id(self, artist_id, artist_name, tracks):
-        # TODO: use tracks table
-        print([artist_id, artist_name, tracks])
+    def processTracksForInsertion(self, tracks: list):
+        processed_tracks = []
+        for track in tracks:
+            processed_track = {
+                'id': track['id']
+            }
+    def setTracksByArtistId(self, artist_meta: dict, tracks: list) -> bool:
+        artist_id = artist_meta['id']
+        artist_name = artist_meta['name']
         try:
-            # serialize tracks
-            # todo: do we actually need pickle? how do we want to play with the data? isn't it faster to have SQL sort our data once it gets there?
-            # todo: if so, then focus should be on the db layer, not the api logic - that just fetches, end of story
-            #pickled_tracks = pickle.dumps()
-            self.db_cursor.execute('INSERT INTO artists ("id", "name", "tracks") VALUES (?, ?, ?)', [artist_id, artist_name, tracks])
+            self.db_cursor.execute('SELECT id from artists WHERE id = ?', artist_id)
+            data = self.db_cursor.fetchone()
+            if data is None:
+                raise LookupError("Artist doesn't currently exist - create the new artist first.")
+            # todo: at what point do we take this and just django it?
+            self.db_cursor.execute('INSERT INTO tracks '
+                                   'VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',)
             self.db.commit()
             return True
         except sqlite3.IntegrityError:
             print('ERROR: artist ' + artist_name + 'already exists.')
             return False
+            # serialize tracks
+            # todo: do we actually need pickle? how do we want to play with the data? isn't it faster to have SQL sort our data once it gets there?
+            # todo: if so, then focus should be on the db layer, not the api logic - that just fetches, end of story
+            #pickled_tracks = pickle.dumps()
+            #self.db_cursor.execute('INSERT INTO artists ("id", "name", "tracks") VALUES (?, ?, ?)', [artist_id, artist_name, tracks])
+            # 1. check if the artist exists
+
 
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os, pathlib
 from db import DB
-import tracks, lyrics, db, analysis
-
+from track import TrackClient
 # read .env
 dotenv = pathlib.Path(os.getcwd() + '/.env')
 if dotenv.is_file():
@@ -19,8 +18,6 @@ SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
 SPOTIFY_CLIENT_SECRET = os.environ['SPOTIFY_CLIENT_SECRET']
 GENIUS_API_KEY = os.environ['GENIUS_API_KEY']
 
+# initialize our database and client for getting tracks
 database = DB()
-sp = tracks.init(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
-sufjan_id, sufjan_name, sufjan_tracks = tracks.get_all_tracks_by_artist(sp, query='sufjan stevens')
-database.set_tracks_for_artist_id(sufjan_id, sufjan_name, sufjan_tracks)
-print(database.get_tracks_by_artist_id(sufjan_id))
+track_client = TrackClient(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
